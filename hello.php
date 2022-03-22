bab971f7239d329e40c6e01cd9bac496cf920f90
iprash155 <prashantprajapati159@gmail.com>
2022-03-22 13:32:13 +0530

    associative array practised

--- hello.php
+++ hello.php
@@ -1,8 +1,16 @@
 <?php 
-        $arr=array('a','b','c','d');
-        foreach($arr as $num){
-              echo $num." ";
-        }
-
+      $students = array(
+                  "prashant"=>array("physics"=>98,"chemistry"=>97,"maths"=>97),
+                  "meet"=>array("physics"=>84,"chemistry"=>97,"maths"=>78),
+                  "visharg"=>array("physics"=>95,"chemistry"=>96,"maths"=>97),
+      );
+      
+      foreach ($students as $student_name => $subjects) {
+            foreach ($subjects as $subject_name => $marks) {
+                  echo $student_name." has scored ".$marks." in ".$subject_name. "\r\n";
+            }
+      }
+        
+        
 ?>
   
