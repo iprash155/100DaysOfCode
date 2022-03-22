0bbc18d7fcc6e57c04aa8851fc4248d33e6ed2d6
iprash155 <prashantprajapati159@gmail.com>
2022-03-22 12:11:42 +0530

    php file added and html basics got placed in learning folder

---
+++ hello.php
@@ -0,0 +1,13 @@
+<?php 
+        $num=1;
+        while($num<=10){
+              $num++;
+               
+               if ($num==5) {
+                     break;
+               }
+               echo $num*2;
+        }
+        
+?>
+  
