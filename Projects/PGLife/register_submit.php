5d8656135e6e67ac085751d3f13a0f6efe9b6c34
iprash155 <prashantprajapati159@gmail.com>
2022-03-24 14:11:16 +0530

    connection established with the test database on mysql

--- Projects/PGLife/register_submit.php
+++ Projects/PGLife/register_submit.php
@@ -1,10 +1,37 @@
 <?php
 
+
+    $db_hostname="127.0.0.1";
+    $db_username="root";
+    $db_password="";
+    $db_name="test";
+
+    // establishing connnection with mysql server
+    $conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name)
+    
+    //checking if connection is established
+    if (!conn) {
+        echo "connection failed: ".mysqli_connect_error();
+        exit;
+    }
+
     $name=$_POST['name'];
     $email=$_POST['email'];
     $password=$_POST['password'];
 
-    echo $name."  ";
-    echo $email."  ";
-    echo $password."  ";
+
+    // query to insert a row in table
+    $sql="insert into test (name,email,password) values ('$name','$email','password')";
+
+    //executing query
+    $result=mysqli_query($conn,$sql)
+
+    //checking if sql query inserts the data
+    if (!$result) {
+        echo "Error: ".mysqli_error();
+        exit;
+    }
+
+    echo "registration successful";
+    mysqli_close($conn);
 ?>
