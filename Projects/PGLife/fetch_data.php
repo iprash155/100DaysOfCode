0854556e3f96644a0f904503e7ae43bd0cee17cc
iprash155 <prashantprajapati159@gmail.com>
2022-03-25 22:36:48 +0530

    fetch_data.php created to fetch data from database test and table users using php function

---
+++ Projects/PGLife/fetch_data.php
@@ -0,0 +1,30 @@
+<?php
+
+    $db_hostname="127.0.0.1";
+    $db_username="root";
+    $db_password="";
+    $db_name="users";
+
+    //connecting to database
+    $conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
+
+    //checking connection and if not connected displaying error message
+    if (!$conn) {
+        echo "connection failed ".mysqli_connect_error();
+    }
+
+    //fetching data from table
+    $sql="select * from users";
+
+    $result=mysqli_query($conn,$sql)
+
+    //displaying error message in  query execution if there is any
+    if (!$result) {
+        echo "Error : ".mysqli_error(); 
+    }
+
+    while ($row=mysqli_fetch_assoc($result)) {
+        echo $row.<br>;
+    }
+    mysqli_close();
+?>
