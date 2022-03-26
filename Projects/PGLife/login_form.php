75a778ce85372532f53effb55a9349f2c857e34a
iprash155 <prashantprajapati159@gmail.com>
2022-03-26 10:46:29 +0530

    login form modified

--- Projects/PGLife/login_form.php
+++ Projects/PGLife/login_form.php
@@ -1,5 +1,7 @@
+<h1>login </h1>
 <form action="login_submit.php" method="post">
-    EMAIL-ID:<input type="text" name="email" placeholder="enter your email-id here"> <br>
-    PASSWORD:<input type="password" name="password" placeholder="enter your password here"><br>
+
+    Email : <input type="text" name="email" placeholder="enter your email-id here"> <br><p></p>
+    Password : <input type="password" name="password" placeholder="enter your password here"><br><p></p>
     <input type="submit" name="submit" value="submit">
 </form>
