57d6a5080571b64d6f5a8333be3bc5f26cd5206b
iprash155 <prashantprajapati159@gmail.com>
2022-03-27 11:38:36 +0530

    dashboard if condition added

--- learning/php/dashboard.php
+++ learning/php/dashboard.php
@@ -3,12 +3,22 @@
     session_start();
 
     //accessign session
-    $user_id=$_SESSION['user_id'];
-    $name=$_SESSION['name'];
+    if (isset($_SESSION['user_id'])) {
+        $user_id=$_SESSION['user_id'];
+        $name=$_SESSION['name'];
+        echo "hello ".$name;
+        
+        //logout
+        ?>
+        <a href="logout.php">logout</a>
+        <?php
+        
+    }
 
     //accessing cookie
     // $user_id=$_COOKIE['user_id'];
     // $name=$_COOKIE['name'];
 
-    echo "hello ".$name;
+
+
 ?>
