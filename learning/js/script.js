f90d4b27275ab1ab6384b78ecd9837e36ba8c721
iprash155 <prashantprajapati159@gmail.com>
2022-03-31 13:47:07 +0530

    conditions and switch statement practised

--- learning/js/script.js
+++ learning/js/script.js
@@ -1,5 +1,7 @@
-"use strict"
-let a = 5;
-document.write(typeof a);
-a = "hi prashant";
-document.write(typeof a);
+"use strict";
+let a = 9 ;
+if (a%2==0) {
+    document.write("it is even no");
+} else {
+    document.write("it is odd no");
+}
