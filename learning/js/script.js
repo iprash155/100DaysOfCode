52affd24ee22662e6a0a31e139a130a549e4b450
iprash155 <prashantprajapati159@gmail.com>
2022-04-02 12:40:06 +0530

    for-in property

--- learning/js/script.js
+++ learning/js/script.js
@@ -1,22 +1,20 @@
 "use strict";
-
-//function construction
-class person {
-    constructor(person_name, person_age) {
-        this.name = person_name;
-        this.age = person_age;
-
+class person{
+    constructor(person_name,person_age){
+        this.name=person_name;
+        this.age=person_age;
     }
     display(){
         document.write(this.name+" is of age "+this.age);
     }
-};
+}
 
-// creating objects using new keyword
+let person1 = new person('prashant',21);
 
-let person1 = new person ('prashant','21');
-let person2 = new person ('himanshu','22');
-
-document.write(person1.name)
-// displayig property
 person1.display();
+
+//for-in loop
+
+for(let property in person1){
+    document.write("<br>"+property + " : "+person1[property]+ "<br>");
+}
