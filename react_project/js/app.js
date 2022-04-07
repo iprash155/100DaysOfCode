a563b51742723af7099be0e954202bbe6eae7117
iprash155 <prashantprajapati159@gmail.com>
2022-04-07 14:37:48 +0530

    component created

--- react_project/js/app.js
+++ react_project/js/app.js
@@ -1,10 +1,15 @@
-const heading = React.createElement(
-    'h1',
-    {id:'abc'},
-    'this is heading'
-);
+
+function Box() {
+    return(
+        <div className='box'>
+            <h1 id='abc'>this is heading</h1>
+            <p>this is paragraph using jsx</p>
+        </div>
+    );
+    
+}
 
 ReactDOM.render(
-    heading,
+    <Box/>,
     document.getElementById('react-container')
 );
