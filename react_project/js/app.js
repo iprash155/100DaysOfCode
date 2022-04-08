c8440ca555e1d4c826ea58e58876cb0c138ad2b6
iprash155 <prashantprajapati159@gmail.com>
2022-04-08 12:19:02 +0530

    component used again with some more functionallity

--- react_project/js/app.js
+++ react_project/js/app.js
@@ -1,4 +1,4 @@
-class Heading extends React.Component{
+class Box extends React.Component{
             // functionallity to the element
     state={color:'black'}; // state
 
@@ -8,16 +8,45 @@ class Heading extends React.Component{
             // returning an ele
     render(){
         return(
-            <div className = 'heading'>
-                <h1 className={this.state.color}> this is heading </h1>
+            <div className = 'box'>
+                <h1 className={this.state.color}> {this.props.heading} heading </h1>
+                <p>this is para</p>
                 <button onClick = {()=>this.changeColor('red')}> Red </button>
+                <button onClick = {()=>this.changeColor('green')}> Green </button>
+                <button onClick = {()=>this.changeColor('blue')}> BLue </button>
+                <button onClick = {()=>this.changeColor('yellow')}> Yellow </button>
             </div>
         );
     }
 }
 
+const App = ()=>{
+    return(
+        <div className="row">
+            <div className="col">
+                <Box heading="first"/>
+            </div>
+            <div className="col">
+                <Box heading="second"/>
+            </div>
+            <div className="col">
+                <Box heading="third"/>
+            </div>
+            <div className="col">
+                <Box heading="forth"/>
+            </div>
+            <div className="col">
+                <Box heading="fifth"/>
+            </div>
+            <div className="col">
+                <Box heading="sixth"/>
+            </div>
+        </div>
+    );
+}
+
 ReactDOM.render(
-    <Heading/>,
+    <App/>,
     document.getElementById('react-container')
 );
 
